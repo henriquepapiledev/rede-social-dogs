a5ad772357e342f625003b59e7d782844fc32001
Henrique Papile <hpapile@gmail.com>
2025-02-21 17:25:29 -0300

    criação da pagina login criar

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -5,6 +5,12 @@ const types = {
     regex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
     message: 'Preencha um email válido',
   },
+  // password: {
+  //   regex:
+  //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+  //   message:
+  //     'A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 dígito. Com no mínimo 8 caracteres.',
+  // },
 };
 
 const useForm = (type) => {
