be0045f9bd9ad7073b3a29be81ab657ffc0dbcfa
Henrique Papile <hpapile@gmail.com>
2025-02-25 18:20:19 -0300

    Criação da página de feed

---
+++ src/Components/Feed/FeedPhotos.jsx
@@ -0,0 +1,36 @@
+import FeedPhotosItem from './FeedPhotosItem';
+import useFetch from '../../Hooks/useFetch';
+import { useEffect } from 'react';
+import { PHOTOS_GET } from '../../Api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import styles from './FeedPhotos.module.css';
+
+const FeedPhotos = () => {
+  const { data, loading, error, request } = useFetch();
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const { response, json } = await request(url, options);
+      console.log(json);
+    };
+    fetchPhotos();
+  }, [request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <div>
+        <ul className={`${styles.feed} animeLeft`}>
+          {data.map((photo) => (
+            <FeedPhotosItem key={photo.id} photo={photo} />
+          ))}
+        </ul>
+      </div>
+    );
+  else return null;
+};
+
+export default FeedPhotos;
