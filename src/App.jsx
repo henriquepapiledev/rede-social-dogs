71496db12a5658d6c65bf4ad3a8a1b9989c11ce7
Henrique Papile <hpapile@gmail.com>
2025-02-27 12:26:24 -0300

    Finalização da página de foto

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
                 </ProtectedRoute>
               }
             />
+            <Route path="foto/:id" element={<Photo />} />
           </Routes>
           <Footer />
         </UserStorage>
