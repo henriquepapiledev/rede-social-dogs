7f2a0158f35a8a9d420551cbf35089ba3d13fc72
Henrique Papile <hpapile@gmail.com>
2025-02-28 11:34:52 -0300

    Finalização css do footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,13 @@
 import styles from './Footer.module.css';
+import Dogs from '../Assets/dogs-footer.svg?react';
 
 const Footer = () => {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </footer>
+  );
 };
 
 export default Footer;
